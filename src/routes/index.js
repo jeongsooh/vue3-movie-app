ada43719f6f0be8942ceab6c5532b0f5b8df1ab6
jeongsooh <jeongsooh@hotmail.com>
2021-12-31 14:54:59 +0900

    기능 모사 완료

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router"
 import Home from './Home'
 import Movie from './Movie'
 import About from './About'
+import NotFound from './NotFound'
 
 export default createRouter({
   // hash
@@ -14,12 +15,16 @@ export default createRouter({
       component: Home
     },
     {
-      path: '/movie',
+      path: '/movie/:id',
       component: Movie
     },
     {
       path: '/about',
       component: About
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
     }
   ]
 })
